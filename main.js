7b4ec9b66cebcf6cfc2a3c6e80301684306b1361
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-04-16 23:48:01 +0900

    マウスオーバーを外れると表示しない

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const imgElement = document.querySelector("img"); // 画像
 const canvas = document.createElement("canvas"); // canvas要素を生成
 const ctx = canvas.getContext("2d"); // 2次元のオブジェクトを作成
+const rectWrapper = document.querySelector(".rect-wrapper");
+const magnification = 1.75;
 
 // 画像が読み込まれたら以下の処理を実行
 imgElement.onload = () => {
@@ -8,15 +10,20 @@ imgElement.onload = () => {
     canvas.height = imgElement.height; // 画像の高さを取得
 
     imgElement.addEventListener("mousemove", (event) => {
+
+        if(document.querySelectorAll('.rect')) {
+            document.querySelectorAll('.rect').forEach(function(elem) {
+                elem.remove();
+            })
+        }
+
         const x = event.offsetX;
         const y = event.offsetY;
-
-        console.log(x);
 
         const centerX = x;
         const centerY = y;
 
-        ctx.drawImage(imgElement, -centerX / 2, -centerY / 2, canvas.width * 1.5, canvas.height * 1.5); // 画像を描画する方法を決定
+        ctx.drawImage(imgElement, -centerX, -centerY, canvas.width * magnification, canvas.height * magnification); // 画像を描画する方法を決定
 
         // canvas要素を画面に表示
         const rectElement = document.createElement("div");
@@ -28,8 +35,16 @@ imgElement.onload = () => {
   position: absolute;
   pointer-events: none;
 `;
-
+rectWrapper.appendChild(rectElement);
         rectElement.appendChild(canvas);
         document.body.appendChild(rectElement);
     });
+
+    imgElement.addEventListener('mouseout', function(event) {
+        if (document.querySelectorAll(".rect")) {
+            document.querySelectorAll(".rect").forEach(function (elem) {
+                elem.remove();
+            });
+        }
+    })
 };
