5dc4fd9aca69ba6a670aa40da76bae1cc3d8be55
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-04-17 01:57:50 +0900

    inputのレンジと連携

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const imgElement = document.querySelector("img"); // 画像
 const canvas = document.createElement("canvas"); // canvas要素を生成
 const ctx = canvas.getContext("2d"); // 2次元のオブジェクトを作成
-const magnification = 2; // 拡大倍率
+
+const value = document.querySelector("#value");
+const input = document.querySelector("#pi_input");
+let magnification = input.value; // 拡大倍率
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+    magnification = input.value;
+    value.textContent = event.target.value;
+});
 
 // 画像が読み込まれたら以下の処理を実行
 imgElement.onload = () => {
@@ -15,27 +23,26 @@ imgElement.onload = () => {
             });
         }
 
-        console.log(event.offsetX);
-
         const imageOffsetX = event.offsetX - (canvas.width - imgElement.width) / 2;
         const imageOffsetY = event.offsetY - (canvas.height - imgElement.height) / 2;
-
-        console.log("元の画像の原点からのX距離" + imageOffsetX);
 
         const centerX = parseInt(imageOffsetX) * magnification;
         const centerY = imageOffsetY * magnification;
 
-        console.log("原点からの距離 * 拡大倍率" + centerX);
+        let positionX;
+        let positionY;
+        if (magnification !== 1) {
+            positionX = -centerX + canvas.width * magnification * 0.5 - canvas.width * 0.5;
+            positionY = -centerY + canvas.height * magnification * 0.5 - canvas.height * 0.5;
+        } else {
+            positionX = -centerX + canvas.width * magnification * 0.5;
+            positionY = -centerY + canvas.height * magnification * 0.5;
+        }
 
-        ctx.drawImage(
-            imgElement,
-            -centerX + canvas.width * magnification * 0.5 - canvas.width * 0.5,
-            -centerY + canvas.height * magnification * 0.5 - canvas.height * 0.5,
-            canvas.width * magnification,
-            canvas.height * magnification
-        ); // 画像を描画する方法を決定
+        // 描画前にcanvasをクリア
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        console.log("描画の位置" + (-centerX + canvas.width * magnification * 0.5));
+        ctx.drawImage(imgElement, positionX, positionY, canvas.width * magnification, canvas.height * magnification); // 画像を描画する方法を決定
 
         // canvas要素を画面に表示
         const rectElement = document.createElement("div");
